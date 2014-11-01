05924b4942f6f614b2856d5bd2aabb525cfee030
sayanpaul <sayan.paul.us@gmail.com>
2014-11-01 16:31:22 -0400

    Finish up code

--- Testing/Img2Ascii.java
+++ Testing/Img2Ascii.java
@@ -3,79 +3,80 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
-
 class Img2Ascii {
 
-
-    BufferedImage img;
-    double pixval;
-    PrintWriter prntwrt;
-    FileWriter filewrt;
-    final String characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
-    final int numQuant = characters.length() - 1;
-    final double incr = 270.0/numQuant;
-    
-
-
-    public Img2Ascii() {
-        try {
-            prntwrt = new PrintWriter(filewrt = new FileWriter("asciiart.txt",true));
-        } catch (IOException ex) {
-        }
-    }
-    
-    public void convertToAscii(String imgname) {
-        try {
-            img = ImageIO.read(new File(imgname));
-        } catch (IOException e) {
-        }
-
-
-        for (int i = 10; i <= img.getHeight(); i+=10)
-        {
-            for (int j = 10; j <= img.getWidth(); j+=10)
-            {
-                Color pixcol = new Color(img.getRGB(j, i));
-                findAverage(img, j, i)
-                pixval = (((pixcol.getRed() * 0.30) + (pixcol.getBlue() * 0.59) + (pixcol.getGreen() * 0.11)));
-                print(strChar(pixval));
-            }
-            try {
-                prntwrt.println("");
-                prntwrt.flush();
-                filewrt.flush();
-            } catch (Exception ex) {
-            }
-        }
-    }
-    
-    public int findAverage(BufferedImage img, int x, int y) {
-      for (int i = x; i < x + 10; i++) {
-         for (int j = y; j < y + 10; j++) {
-         
-         }
+   BufferedImage img;
+   double pixval;
+   PrintWriter prntwrt;
+   FileWriter filewrt;
+   final String characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+   final int numQuant = characters.length() - 1;
+   final double incr = 270.0/numQuant;
+   
+   public Img2Ascii() {
+      try {
+         prntwrt = new PrintWriter(filewrt = new FileWriter("asciiart.txt",true));
+      } 
+      catch (IOException ex) {
       }
-
-
-    public String strChar(double g)
-    {
-        int substr = numQuant - (int)Math.ceil(g/incr);
-        return characters.substring(substr, substr + 1);
-    }
-
-
-    public void print(String str)
-    {
-        try {
-            prntwrt.print(str);
+   }
+   
+   public void convertToAscii(String imgname) {
+      try {
+         img = ImageIO.read(new File(imgname));
+      } 
+      catch (IOException e) {
+      }
+   
+   
+      for (int i = 10; i <= img.getHeight(); i+=10)
+      {
+         for (int j = 10; j <= img.getWidth(); j+=10)
+         {
+            Color pixcol = new Color(img.getRGB(j, i));
+            print(strChar(findAverage(img, j, i)));
+         }
+         try {
+            prntwrt.println("");
             prntwrt.flush();
             filewrt.flush();
-        } catch (Exception ex) {
-        }
-    }
-    
-    public static void main(String[] args) {
-        Img2Ascii obj=new Img2Ascii();
-        obj.convertToAscii("icon_smile2.png");
-    }
+         } 
+         catch (Exception ex) {
+         }
+      }
+   }
+   
+   public double findAverage(BufferedImage img, int x, int y) {
+      double total = 0.;
+      for (int i = x; i < x + 10; i++) {
+         for (int j = y; j < y + 10; j++) {
+            if (i <= img.getWidth() && y <= img.getHeight())
+               total += (((pixcol.getRed() * 0.30) + (pixcol.getBlue() * 0.59) + (pixcol.getGreen() * 0.11)));
+         }
+      }
+      return total / 100;
+   }
+
+   public String strChar(double g)
+   {
+      int substr = numQuant - (int)Math.ceil(g/incr);
+      return characters.substring(substr, substr + 1);
+   }
+
+
+   public void print(String str)
+   {
+      try {
+         prntwrt.print(str);
+         prntwrt.flush();
+         filewrt.flush();
+      } 
+      catch (Exception ex) {
+      }
+   }
+   
+   public static void main(String[] args) {
+      Img2Ascii obj=new Img2Ascii();
+      obj.convertToAscii("icon_smile2.png");
+   }
 }
