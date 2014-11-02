4ba8227c082a07448380b75e1651f27ec29a615c
sayanpaul <sayan.paul.us@gmail.com>
2014-11-01 21:22:44 -0400

    Finish imager.js

--- imager.js
+++ imager.js
@@ -1,8 +1,3 @@
-import java.awt.Color;
-import java.awt.image.*;
-import java.io.*;
-import javax.imageio.*;
-
 const var characters = "œæ©¢øðþãêõûµ±¤ç¿ª®¦¡«‹—¬-“”„°¯¨…•ˆ‘’‚'()   ";
 const var numQuant = characters.length() - 1;
 const var incr = 270.0/numQuant;
@@ -13,14 +8,15 @@ var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
 context.drawImage(img, 0, 0);
 
-var ascii = function(img) {
-   String ascii = '';
+var ascii = function(img) { //TODO: send this ascii to the other person
+   String ascii = "";
    var avg;
+   var substr;
    for (var i = 0; i <= img.height; i+=10) {
       for (var j = 0; j <= img.width; j+=10) {
          avg = findAverage(img, j, i);
-         
-         ascii += strChar(findAverage(img, j, i));
+         substr = Math.floor(avg/incr);
+         ascii += characters.substring(substr, substr + 1);
       }
    }
    return ascii;
@@ -38,25 +34,4 @@ var average = function(img, x, y) {
    for (var i = 0, n = data.length; i < n; i += 4)
       total += ((data[i] * 0.33) + (data[i + 1] * 0.33) + (data[i + 2] * 0.33)));
    return total / 100;
-}
-
-var strChar = function(g) {
-   var substr = Math.floor(g/incr);
-   return characters.substring(substr, substr + 1);
-}
-
-public void print(String str)
-{
-   try {
-      prntwrt.print(str);
-      prntwrt.flush();
-      filewrt.flush();
-   } 
-   catch (Exception ex) {
-   }
-}
-
-public static void main(String[] args) {
-   Img2Ascii obj=new Img2Ascii();
-   obj.convertToAscii("Photo on 11-1-14 at 4.07 PM.jpg");
 }
