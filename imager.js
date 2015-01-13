4efb7c55b5d84f6374e69922eedf3aa189403e15
Shrey Gupta <gupta.lego@gmail.com>
2015-01-13 08:44:14 -0500

    Update imager.js

--- imager.js
+++ imager.js
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
-const var characters = "œæ©¢øðþãêõûµ±¤ç¿ª®¦¡«‹—¬-“”„°¯¨…•ˆ‘’‚'()   ";
+const var characters = "@80GCLft1i;:,.";
 const var numQuant = characters.length() - 1;
 const var incr = 255.0/numQuant;
 var totDens = 0.;
